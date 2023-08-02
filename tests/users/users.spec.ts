6f8c0bc760c2cfeb6e2a1cbca4269cbad3dc5550
Lucas da Mata <lucasmsilva_022@outlook.com>
2023-08-01 22:04:16 -0300

    test: implement TDD to create user

--- tests/users/users.spec.ts
+++ tests/users/users.spec.ts
@@ -1,23 +1,54 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
-import supertest from 'supertest'
+import { UserFactory } from 'Database/factories'
 
-const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
-
-test.group('User', () => {
-  test('it should create an user', async ({ assert }) => {
+test.group('User', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+  test('it should create an user', async ({ assert, client }) => {
     const userPayload = {
       email: 'test@example.com',
       username: 'test',
       password: 'test',
       avatar: 'https://iamges.com/images/1',
     }
-    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    const response = await client.post('/users').json(userPayload)
 
-    assert.exists(body.user, 'User undefined')
-    assert.equal(body.user.id, 'Id undefined')
-    assert.equal(body.user.email, userPayload.email)
-    assert.equal(body.user.username, userPayload.username)
-    assert.equal(body.user.password, userPayload.password)
-    assert.equal(body.user.avatar, userPayload.avatar)
+    const { password, avatar, ...expected } = userPayload
+
+    response.assertStatus(201)
+    response.assertBodyContains({ user: expected })
+    assert.notExists(response.body().user.password, 'Password defined')
+  })
+
+  test('it should return 409 when email is already in use', async ({ assert, client }) => {
+    const { email } = await UserFactory.create()
+    const response = await client.post('/users').json({
+      email,
+      username: 'test',
+      password: 'test',
+    })
+    response.assertStatus(409)
+  })
+
+  test('it should return 409 when username is already in use', async ({ assert, client }) => {
+    const { username } = await UserFactory.create()
+    const response = await client.post('/users').json({
+      username,
+      email: 'test@example.com',
+      password: 'test',
+    })
+    response.assertStatus(409)
+    assert.exists(response.body().message)
+    assert.exists(response.body().code)
+    assert.exists(response.body().status)
+  })
+
+  test('it should return 422 when required data is not provided', async ({ assert, client }) => {
+    const response = await client.post('/users').json({})
+    assert.equal(response.body().code, 'BAD_REQUEST')
+    response.assertStatus(422)
   })
 })
